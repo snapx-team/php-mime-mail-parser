cf460b173338ce476f45b0e94335fae3711f1ff7
Vincent Dauce <exorus.spam@gmail.com>
2020-08-12 18:05:05 +0200

    wip

---
+++ src/Bou.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PhpMimeMailParser;
+
+final class Bou
+{
+    protected $flag;
+
+    public function getFlag()
+    {
+        return $this->flag;
+    }
+
+    public function setFlag($flag)
+    {
+        return $this->flag = $flag;
+    }
+}
